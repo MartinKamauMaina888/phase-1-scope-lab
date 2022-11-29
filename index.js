0d4c9980843cea6d19f5e0b49ec07e0a1609bccc
MartinKamauMaina888 <martin.maina01@student.moringaschool.com>
2022-11-29 11:47:33 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,67 @@
 // Write your solution in this file!
+
+constants
+const x= 40;
+
+
+// Number 1
+
+var customerName="kamau"
+
+// Number 2
+
+function upperCaseCustomerName()
+
+{
+    return customerName.toUpperCase()
+}
+
+console.log(upperCaseCustomerName())
+
+// Number 3
+
+var bestCustomer="";
+
+function setBestCustomer()
+
+{
+    bestCustomer='not bob'
+}
+
+setBestCustomer()
+
+console.log(bestCustomer);
+
+// Number 4
+
+function overwriteBestCustomer()
+
+{
+    var bestCustomer = "New Name"
+
+    return bestCustomer
+}
+
+console.log(overwriteBestCustomer());
+
+// Number 5
+
+function leastFavoriteCustomer()
+
+{
+    console.log("Value of x is:",x);
+}
+
+leastFavoriteCustomer()
+
+// Number 6
+function changeLeastFavoriteCustomer()
+{
+
+    x=50;
+    
+    console.log("Value of new changed x is:",x);
+
+}
+
+changeLeastFavoriteCustomer()
